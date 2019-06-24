0fa8b31e932a6a0306c2f036c38327df9e2ff499
xmq291 <xmq291@gmail.com>
2019-06-24 18:48:26 -0400

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -15,6 +15,7 @@ import { switchMap } from 'rxjs/operators';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  errMess: string;
   dishIds: string[];
   prev: string;
   next: string;
@@ -50,7 +51,8 @@ export class DishdetailComponent implements OnInit {
       .subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params
       .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); }, 
+        errmess => this.errMess = <any>errmess );
   }
 
   setPrevNext(dishId: string) {
